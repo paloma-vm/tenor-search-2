ffd21bbc3087fd88cb68bf5265e07ba69e56acfe
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-04-12 20:23:49 -0700

    added search form

--- app.js
+++ app.js
@@ -22,16 +22,21 @@ app.set('views', './views');
 
 // Routes
 
+app.get('/', (req, res) => {
+  console.log(req.query)
+  res.render('home')
+})
+
 // app.get('/', (req, res) => {
 //     res.send('Hello Squirrel');
 //   });
 
-app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
-})
+// app.get('/', (req, res) => {
+//   // set the url of the gif
+//   const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+//   // render the hello-gif view, passing the gifUrl into the view to be displayed
+//   res.render('hello-gif', { gifUrl })
+// })
 
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
